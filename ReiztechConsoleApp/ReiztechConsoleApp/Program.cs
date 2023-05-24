f2392f2a1ea1bb9d0946b858daef65e5a130b2ca
Rhico Abueme <rhicoabuemegithub@gmail.com>
2023-05-24 19:00:46 +0800

    create solution for Problem 2.

--- ReiztechConsoleApp/ReiztechConsoleApp/Program.cs
+++ ReiztechConsoleApp/ReiztechConsoleApp/Program.cs
@@ -37,16 +37,108 @@ if(val == "a")
 
 // Tree Problem
 else if(val == "b")
-{
+{       
+        //  Sample Tree:
+        /// Root
+        /// |
+        /// Child 1--------------------------------------Child 2
+        ///     |                   /                       |                   \
+        /// Grandchild11       Grandchild12           Grandchild22            Grandchild32
+        ///                         |                   /       \               
+        ///                    Grandchild112   Grandchild122   Grandchild222
+        ///                                          |
+        ///                                    Grandchild1122
 
+
+        Branch<string> root = new Branch<string>("Root");
+        
+        Branch<string> child1 = new Branch<string>("Child 1");
+        Branch<string> child2 = new Branch<string>("Child 2");
+
+        Branch<string> grandchild11 = new Branch<string>("Grandchild 1 - 1");
+        Branch<string> grandchild12 = new Branch<string>("Grandchild 1 - 2");
+        Branch<string> grandchild22 = new Branch<string>("Grandchild 2 - 2");
+        Branch<string> grandchild32 = new Branch<string>("Grandchild 3 - 2");
+
+        Branch<string> grandchild112 = new Branch<string>("Grandchild 1 - 1 - 2");
+        Branch<string> grandchild122 = new Branch<string>("Grandchild 1 - 2 - 2");
+        Branch<string> grandchild222 = new Branch<string>("Grandchild 2 - 2 - 2");
+
+        Branch<string> grandchild1122 = new Branch<string>("Grandchild 1 - 1 - 2 - 2");
+
+
+        root.AddChild(child1);
+        root.AddChild(child2);
+
+        child1.AddChild(grandchild11);
+        child2.AddChild(grandchild12);
+        child2.AddChild(grandchild22);
+        child2.AddChild(grandchild32);
+
+        grandchild12.AddChild(grandchild112);
+        grandchild22.AddChild(grandchild122);
+        grandchild22.AddChild(grandchild222);
+
+        grandchild122.AddChild(grandchild1122);
+
+        // Perform operations on the tree
+        TraverseTree(root);
+
+        // Calculate the depth of the tree
+        int depth = CalculateTreeDepth(root);
+
+        Console.WriteLine("The depth of the tree is: " + depth);
 }
 else
 {
     Console.WriteLine("Please enter correct choice.");
 }
 
-class Branch 
+static void TraverseTree(Branch<string> node)
 {
-    List<Branch> branches;
+    Console.WriteLine(node.Value);
+
+    foreach (var child in node.Children)
+    {
+        TraverseTree(child);
+    }
+}
+
+static int CalculateTreeDepth(Branch<string> node)
+{
+    if (node.Children.Count == 0)
+    {
+        return 1; // Leaf node has depth 1
+    }
+
+    int maxChildDepth = 0;
+
+    foreach (var child in node.Children)
+    {
+        int childDepth = CalculateTreeDepth(child);
+        maxChildDepth = Math.Max(maxChildDepth, childDepth);
+    }
+
+    return maxChildDepth + 1; // Add 1 for the current node
+}
+
+public class Branch<T>
+{
+    public T Value { get; set; }
+    public Branch<T>? Parent { get; set; }
+    public List<Branch<T>> Children { get; set; }
+
+    public Branch(T value)
+    {
+        Value = value;
+        Parent = null;
+        Children = new List<Branch<T>>();
+    }
+
+    public void AddChild(Branch<T> child)
+    {
+        child.Parent = this;
+        Children.Add(child);
+    }
 }
 
